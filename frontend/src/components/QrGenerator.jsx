a246a8562c78b5cd1d45d3d0ba702e858867aaaf
Arpit Panchal <arpitpanchal2914@gmail.com>
2024-12-30 16:02:50 +0530

    add

--- frontend/src/components/QrGenerator.jsx
+++ frontend/src/components/QrGenerator.jsx
@@ -7,31 +7,39 @@ const QrGenerator = () => {
     <>
       <div className="main_container">
           <div className="input_container">
-            <input
-              type="text"
-              placeholder="Type..."
-              onChange={(e) => setText(e.target.value)}
-              value={text}
-            />
+              <textarea
+                  value={text}
+                  onChange={(e) => setText(e.target.value)}
+                  placeholder="Type something here..."
+                  rows={5}   // Fixed number of rows
+                  cols={40}
+               />
           </div>
+             
 
-          <div className="content_container">
-            {
-            text ?(
-              <QRCode
-                value={text} // વપરાશકર્તા દાખલ કરેલું ટેક્સ્ટ અથવા URL
+          <div className="content_container"> 
+            <div className='qr_container'>
+              {
+              text ?(
+                <QRCode
+                  value={text} // વપરાશકર્તા દાખલ કરેલું ટેક્સ્ટ અથવા URL
+                  size={200} // QR Codeનું કદ
+                  level="H" // High-level Error Correction
+                  
+                />
+              ) : (
+                <QRCode
+                value={"Hare Krishna :)"} // વપરાશકર્તા દાખલ કરેલું ટેક્સ્ટ અથવા URL
                 size={200} // QR Codeનું કદ
                 level="H" // High-level Error Correction
+                
               />
-            ) : (
-              <QRCode
-              value={"Hare Krishna :)"} // વપરાશકર્તા દાખલ કરેલું ટેક્સ્ટ અથવા URL
-              size={200} // QR Codeનું કદ
-              level="H" // High-level Error Correction
-            />
-            )}
+              )}
+            </div>
+            
           <h3>Scan And Check Result :)</h3>
           </div>
+          
       </div>
     </>
   )
